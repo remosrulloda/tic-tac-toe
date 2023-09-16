75ef5931e8ef6432dce6afc91f7faa252c630352
remosrulloda <remosrulloda@gmail.com>
2023-09-16 12:32:09 -0700

    files

---
+++ index.js
@@ -0,0 +1,18 @@
+// let gameBoard = document.querySelector('.container');
+let squares = document.querySelectorAll('.square');
+
+const gameBoard = () => {
+    let gameBoardArr = ['', '', '', '', '', '', '', '', ''];
+
+    const render = () => {
+        let boardHTML = '';
+
+    }
+
+};
+
+
+
+const displayController = () => {
+
+};
