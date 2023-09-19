a33c46880b7b030b92671dcef4a1479618769701
Remo Rulloda <remosrulloda@gmail.com>
2023-09-18 19:39:53 -0700

    adding

--- index.js
+++ index.js
@@ -1,19 +1,55 @@
-let gameBoard = document.querySelector('.gameBoard');
-let squares = document.querySelectorAll('.square');
+const startBtn = document.querySelector('#startBtn');
+const restartBtn = document.querySelector('#restartBtn');
 
-const Gameboard = () => {
+// Gameboard
+const Gameboard = (() => {
     let gameBoard = ['', '', '', '', '', '', '', '', ''];
 
     const render = () => {
         let boardHTML = '';
         gameBoard.forEach((square, index) => {
             boardHTML += `<div class='square' id='square-${index}'>${square}</div>`;
-
         })
+        document.querySelector('.gameBoard').innerHTML = boardHTML;
     }
 
-    gameBoard.innerHTML = boardHTML;
-};
+    return {
+        render
+    }
+})(); // IIFE
 
-const startBtn = document.querySelector('#startBtn');
-const restartBtn = document.querySelector('#restartBtn');
+
+// Function that creates the players
+const createPlayer = (name, mark) => {
+    return {
+        name,
+        mark
+    }
+}
+
+
+// Game
+const Game = (() => {
+    let players = [];
+    let currentPlayerIndex = 0;
+    let gameOver = false;
+
+    const start = () => {
+        players = [
+            createPlayer(document.querySelector('#player1').value, 'X'),
+            createPlayer(document.querySelector('#player2').value, 'O')
+        ];
+        currentPlayerIndex = 0;
+        gameOver = false;
+        Gameboard.render();
+    }
+
+    return {
+        start
+    }
+})(); // IIFE
+
+
+startBtn.addEventListener('click', () => {
+    Game.start();
+});
